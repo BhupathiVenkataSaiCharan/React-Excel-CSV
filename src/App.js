9ea1ac0f229e5f3bee352170e4098e6ae1646b78
BhupathiVenkataSaiCharan <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2021-11-26 14:31:52 +0530

    data update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Customers } from './Customers'
+import { Header } from './Header'
+import { ExportReactCSV } from './ExportReactCSV'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  customers = () => {
+    let custs = [
+      {
+        firstName : `charan`,
+        lastName : `sai`,
+        email : `saicharan12599@gmail.com`, 
+        address:`hyderabad`, 
+        zipcode:`500084`
+      }
+      
+    ]
+    // for (let i = 0; i <= 25; i++) {
+    //   custs.push({firstName: `first${i}`, lastName: `last${i}`,
+    //   email: `abc${i}@gmail.com`, address: `000${i} street city, ST`, zipcode: `0000${i}`});
+    // }
+    return custs;
+  }
+  
+  render() {
+
+    return (
+      <div className="App">
+        <Header />
+          <div className="row">
+              <div className="col-md-8">
+                <h2>Customers</h2>
+              </div>
+              <div className="col-md-4 center">
+                <ExportReactCSV csvData={this.customers()} fileName={this.props.fileName} />
+              </div>
+          </div>
+        <Customers customers={this.customers()}/>
+      </div>
+    );
+  }
 }
 
 export default App;
